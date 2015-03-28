353258bd9539914e7a5ad4be3180564935ceedc9
Mohammad Mustafa <mohammadmustafa9s4@gmail.com>
2015-03-28 16:42:18 +0200

    Initial commit

---
+++ app/src/androidTest/java/mohammad9s4/helloworld/helloworld/ApplicationTest.java
@@ -0,0 +1,13 @@
+package mohammad9s4.helloworld.helloworld;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
